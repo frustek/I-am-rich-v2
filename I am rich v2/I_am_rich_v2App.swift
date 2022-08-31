32d44760097468b72ce5097308167102e245b5db
Adam Mroz <MrozAdam1992@gmail.com>
2022-08-31 21:04:25 +0200

    Initial Commit

---
+++ I am rich v2/I_am_rich_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_rich_v2App.swift
+//  I am rich v2
+//
+//  Created by Adam Mroz on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct I_am_rich_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
